5b6bf4f057627827ce15a07db0686bd5dac626aa
Hetav Antani <hetav.antani@gmail.com>
2026-03-01 17:53:24 +0530

    backend start

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { color } from "html2canvas/dist/types/css/types/color";
+
 
 function Login({ onLogin, goRegister }) {
   const [email, setEmail] = useState(localStorage.getItem("email") || "");
